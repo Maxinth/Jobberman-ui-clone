be50a1f88f24a18f2b3c22322e8fe13e626f2493
maxinth <tunde.j.adeleye@gmail.com>
2021-02-03 03:27:32 +0100

    Generated Testimonials

---
+++ src/components/StandOut/testimonialData.js
@@ -0,0 +1,24 @@
+// import me from '../../../testimonials'
+export const data = [
+  {
+    img: "../../../testimonials/robert-orere.jpg",
+    name: "robert orere",
+    position: "Human Resource Senior Advisor - GIZ",
+    testimonial:
+      "Jobberman has made our recruitment process a lot easier and efficient. Overtime, we have gotten a number of qualified candidates through Jobberman",
+  },
+  {
+    img: "../../../testimonials/babatunde-ogunyemi.jpg",
+    name: "babatunde ogunyemi",
+    position: "HR Manager - StarTimes Nigeria",
+    testimonial:
+      "We used the Pro Recruit product. The quality of candidates we received have been awesome, as well as quality of CVs. Jobberman seems to get us what we need, at the right time - so there’s no need for us to start going through CVs, they just give us what we need. Before Jobberman, you’d probably rely on referrals, and most times you don’t get the best from that. I will definitely use Jobberman ",
+  },
+  {
+    img: "../../../testimonials/vayen-collins-amaife.jpg",
+    name: "Vayen Collins-Amaife ",
+    position: "HR Manager - Tek-Experts Nigeria",
+    testimonial:
+      "We made us of the Recruitment Services, we needed to hire 200 engineers in a short period of time. I believe we received the best crop of engineers in the market. Jobberman did all the pre-interviews, scheduling, pre-assessment and selection for the candidates for us. They had already pre-selected from the bulk and gave us the most suitable people, then we had to select the best for us. It made things a lot easier. I will use Jobberman again, because you took 60-70% of the stress off us.",
+  },
+];
