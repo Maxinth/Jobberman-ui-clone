bb61cdd478e9ea3302696b686cccc4774968d80b
maxinth <tunde.j.adeleye@gmail.com>
2021-02-13 09:18:40 +0100

    FeaturedProduct first go

--- src/components/EmployersPageComponents/EmployersPage.js
+++ src/components/EmployersPageComponents/EmployersPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Advertise from "./Advertise";
+import FeaturedProducts from "./FeaturedProduct";
 import HelpSection from "./HelpSection";
 import EmployersMainBanner from "./MainBanner";
 import SmallBanner from "./SmallBanner";
@@ -11,6 +12,7 @@ const EmployersPage = () => {
       <EmployersMainBanner />
       <Advertise />
       <HelpSection />
+      <FeaturedProducts />
     </>
   );
 };
---
+++ src/components/EmployersPageComponents/FeaturedProduct/index.js
@@ -0,0 +1,61 @@
+import {
+  HowTosContainer,
+  Heading,
+  Text,
+} from "../../StandOut/standOutElements";
+import { ConnectBox as ProductBox } from "../../Connect/styled";
+import styled from "styled-components";
+const FeaturedContainer = styled(HowTosContainer)`
+  display: block;
+  background-color: rgb(250, 250, 250);
+  padding: 1rem 2rem;
+  ${Heading} {
+    text-align: center;
+    margin: 2rem 0;
+  }
+
+  ${ProductBox} {
+    padding: 1rem 2rem;
+    max-width: 900px;
+    h4 {
+      font-size: 1.7rem;
+    }
+    p + p {
+      margin-top: 3rem;
+    }
+  }
+
+  ${Text} {
+    max-width: 600px;
+    line-height: 1.8;
+    font-size: 1.2rem;
+    /* text-align: left; */
+  }
+`;
+const FeaturedProducts = () => {
+  return (
+    <FeaturedContainer>
+      <Heading>Featured Product</Heading>
+      <ProductBox>
+        <Heading as="h4">
+          We now offer skills assessments when creating a job listing
+        </Heading>
+        <Text>
+          You decrease the time spent on sorting through the huge volume of
+          applications from jobseekers. You can sort through job seekers based
+          on test scores they achieve
+        </Text>
+        <Text>
+          An assessment will not only help you identify candidates that will be
+          a good fit for jobs at your company, but it will also help predict a
+          new hire’s on-the-job performance. It Introduces an element of
+          objectivity into the hiring process by providing standardised results
+          that can be applied to all your applicants.
+        </Text>
+        <Heading as="h4">Assessments available for these categories </Heading>
+      </ProductBox>
+    </FeaturedContainer>
+  );
+};
+
+export default FeaturedProducts;
